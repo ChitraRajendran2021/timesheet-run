251ca9416aefb5388d73ed9eae8d09b871c8515b
Chitra <chitradevi.rajendran@gmai.com>
2022-01-30 12:14:54 +0100

    dddf

--- src/main/java/com/ikea/timesheet/ikea/RestTimeController.java
+++ src/main/java/com/ikea/timesheet/ikea/RestTimeController.java
@@ -3,8 +3,11 @@ package com.ikea.timesheet.ikea;
 import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "https://hello-cloud-run-web-2ia6hqkrja-uc.a.run.app")
 
 @RestController
 public class RestTimeController {
